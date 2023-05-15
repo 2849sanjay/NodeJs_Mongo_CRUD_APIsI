dbe782bccb40fa6ef48d500a8acceaac52d659cb
Sanjay Kumar Verma <2849sanjay@gmail.com>
2023-05-15 08:40:04 +0530

    create and get part

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,11 +3,29 @@ const Product = require("../models/product.model");
 
 
 // add new product
-const addProducts = async (req, res) => { }
-// get all products
+const addProducts = async (req, res) => {
 
+    const createProducts = new Product({
+        title: req.body.title,
+        price: req.body.price,
+        image: req.body.image,
+        details: req.body.details
+
+    })
+    try {
+        const saveProducts = await Product.save()
+        res.send(saveProducts);
+    } catch (err) {
+        res.status(400).send({
+            message: "some internal problem during creating products"
+        })
+    }
+}
+
+
+// get all products
 const getAllProducts = async (req, res) => {
-   
+
     try {
         const products = Product.find();
         res.json(products);
